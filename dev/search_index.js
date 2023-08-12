8e95a909946fe71d12af3153ceaa913701c3ba7b
Documenter.jl <documenter@juliadocs.github.io>
2023-08-12 15:35:12 +0000

    build based on cf97e6c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = EarthData","category":"page"},{"location":"#EarthData","page":"Home","title":"EarthData","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for EarthData.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [EarthData]","category":"page"}]
+}
